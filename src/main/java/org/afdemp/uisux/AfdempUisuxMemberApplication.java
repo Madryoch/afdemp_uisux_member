736c4b4e4c30be41bb71b133ab02fb6e8070c854
padoura <padoura@users.noreply.github.com>
2018-02-07 23:58:05 +0200

    Initialize member portal

---
+++ src/main/java/org/afdemp/uisux/AfdempUisuxMemberApplication.java
@@ -0,0 +1,12 @@
+package org.afdemp.uisux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfdempUisuxMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfdempUisuxMemberApplication.class, args);
+	}
+}
