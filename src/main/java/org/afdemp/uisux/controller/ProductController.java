880172611b663d7ad73ad7740892ea08dded1fbd
padoura <padoura@users.noreply.github.com>
2018-02-08 02:12:09 +0200

    Added a view pending offers button

--- src/main/java/org/afdemp/uisux/controller/ProductController.java
+++ src/main/java/org/afdemp/uisux/controller/ProductController.java
@@ -82,6 +82,14 @@ public class ProductController {
 		return "offerList";
 	}
 	
+	@RequestMapping("/offerList")
+	public String productInfo(Model model, Principal principal) {
+		User user = userService.findByUsername(principal.getName());
+		UserRole userRole = userRoleService.findByUserAndRole(user, "ROLE_MEMBER");
+		ShoppingCart shoppingCart = userRole.getShoppingCart();
+		model.addAttribute("memberCartItemList", memberCartItemService.findByShoppingCart(shoppingCart));
+		return "offerList";
+	}
 	
 	
 	
@@ -103,118 +111,118 @@ public class ProductController {
 	
 	
 
-	@RequestMapping(value = "/add", method = RequestMethod.GET)
-	public String addProduct(Model model) {
-		Product product = new Product();
-		Category category = new Category();
-		List<Category> categoryList = categoryService.findAll();
-		model.addAttribute("product", product);
-		model.addAttribute("category", category);
-		model.addAttribute("categoryList", categoryList);
-		return "addProduct";
-	}
-
-	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public String addProductPost(@ModelAttribute("product") Product product, BindingResult productResult,
-				@ModelAttribute("type") String type, BindingResult typeResult, Model model) {
-
-		if (productResult.hasErrors() || typeResult.hasErrors()) {
-			model.addAttribute("insertSuccess",false);
-			return "addProduct";
-		}
-
-		boolean success;
-		if ((productService.createProduct(product, type)!=null) & ImageUtility.trySaveImage(product)) {
-				success = true;
-		}else{
-			success = false;
-		}
-		
-		product = new Product();
-		List<Category> categoryList = categoryService.findAll();
-		model.addAttribute("categoryList",categoryList);
-		model.addAttribute("insertSuccess",success);
-		return "addProduct";
-	}
-	
-
-	
-	@RequestMapping(value="/remove", method=RequestMethod.POST)
-	public String remove(
-			@ModelAttribute("id") String id, Model model
-			) {
-		productService.removeOne(Long.parseLong(id.substring(8)));
-		List<Product> productList = productService.findAll();
-		model.addAttribute("productList", productList);
-		return "redirect:/product/productList";
-	}
-	
-	
-
-	
-	@RequestMapping("/updateProduct")
-	public String updateProduct(@RequestParam("id") Long id, Model model) {
-		Product product = productService.findOne(id);
-		List<Category> categoryList = categoryService.findAll();
-		model.addAttribute("product", product);
-		model.addAttribute("categoryList", categoryList);
-		return "updateProduct";
-	}
-	
-	@RequestMapping(value="/updateProduct", method=RequestMethod.POST)
-	public String updateProductPost(@ModelAttribute("product") Product product, BindingResult productResult,
-			@ModelAttribute("type") String type, BindingResult typeResult, Model model) {
-		
-		if (productResult.hasErrors() || typeResult.hasErrors()) {
-			return "redirect:/product/productInfo?id="+product.getId();
-		}
-
-		boolean success;
-		if ((productService.createProduct(product, type)!=null) & ImageUtility.trySaveImage(product)) {
-				success = true;
-		}else{
-			success = false;
-		}
-		model.addAttribute("updateSuccess",success);
-		
-		return "redirect:/product/productInfo?id="+product.getId();
-	}
-	
-	
-	@RequestMapping("/toggleProductActive")
-	public String toggleProductActive(@RequestParam("id") String id, Model model) {
-		String productId =id.substring(8);
-		Product product = productService.findOne(Long.parseLong(productId));
-		productService.toggleActive(product);
-		model.addAttribute("product", product);
-		return "productList";
-	}
-	
-	@RequestMapping(value = "/stockUp", method = RequestMethod.GET)
-	public String stockUp(@RequestParam("id") Long id, Model model) {
-		Product product = productService.findOne(id);
-		List<MemberCartItem> memberCartItemList = memberCartItemService.findAllAvailableItems(id);
-		
-		List<MemberCartItemWrapper> wrapperList = new ArrayList<>();
-		for (MemberCartItem memberCartItem : memberCartItemList) {
-			wrapperList.add(new MemberCartItemWrapper(memberCartItem.getShoppingCart().getUserRole().getUser(), memberCartItem)); 
-		}
-		
-		model.addAttribute("product", product);
-		model.addAttribute("wrapperList", wrapperList);
-		return "stockUp";
-	}
-	
-	@RequestMapping(value = "/stockUpConfirm", method = RequestMethod.GET)
-	public String stockUpConfirm(@RequestParam("memberCartItemId") Long memberCartItemId, Model model) {
-		
-		MemberCartItem memberCartItem = memberCartItemService.findById(memberCartItemId);
-		
-		MemberCartItemWrapper memberCartItemWrapper = new MemberCartItemWrapper(memberCartItem.getShoppingCart().getUserRole().getUser(), memberCartItem);
-		
-		model.addAttribute("memberCartItemWrapper", memberCartItemWrapper);
-		return "stockUpConfirm";
-	}
+//	@RequestMapping(value = "/add", method = RequestMethod.GET)
+//	public String addProduct(Model model) {
+//		Product product = new Product();
+//		Category category = new Category();
+//		List<Category> categoryList = categoryService.findAll();
+//		model.addAttribute("product", product);
+//		model.addAttribute("category", category);
+//		model.addAttribute("categoryList", categoryList);
+//		return "addProduct";
+//	}
+//
+//	@RequestMapping(value = "/add", method = RequestMethod.POST)
+//	public String addProductPost(@ModelAttribute("product") Product product, BindingResult productResult,
+//				@ModelAttribute("type") String type, BindingResult typeResult, Model model) {
+//
+//		if (productResult.hasErrors() || typeResult.hasErrors()) {
+//			model.addAttribute("insertSuccess",false);
+//			return "addProduct";
+//		}
+//
+//		boolean success;
+//		if ((productService.createProduct(product, type)!=null) & ImageUtility.trySaveImage(product)) {
+//				success = true;
+//		}else{
+//			success = false;
+//		}
+//		
+//		product = new Product();
+//		List<Category> categoryList = categoryService.findAll();
+//		model.addAttribute("categoryList",categoryList);
+//		model.addAttribute("insertSuccess",success);
+//		return "addProduct";
+//	}
+//	
+//
+//	
+//	@RequestMapping(value="/remove", method=RequestMethod.POST)
+//	public String remove(
+//			@ModelAttribute("id") String id, Model model
+//			) {
+//		productService.removeOne(Long.parseLong(id.substring(8)));
+//		List<Product> productList = productService.findAll();
+//		model.addAttribute("productList", productList);
+//		return "redirect:/product/productList";
+//	}
+//	
+//	
+//
+//	
+//	@RequestMapping("/updateProduct")
+//	public String updateProduct(@RequestParam("id") Long id, Model model) {
+//		Product product = productService.findOne(id);
+//		List<Category> categoryList = categoryService.findAll();
+//		model.addAttribute("product", product);
+//		model.addAttribute("categoryList", categoryList);
+//		return "updateProduct";
+//	}
+//	
+//	@RequestMapping(value="/updateProduct", method=RequestMethod.POST)
+//	public String updateProductPost(@ModelAttribute("product") Product product, BindingResult productResult,
+//			@ModelAttribute("type") String type, BindingResult typeResult, Model model) {
+//		
+//		if (productResult.hasErrors() || typeResult.hasErrors()) {
+//			return "redirect:/product/productInfo?id="+product.getId();
+//		}
+//
+//		boolean success;
+//		if ((productService.createProduct(product, type)!=null) & ImageUtility.trySaveImage(product)) {
+//				success = true;
+//		}else{
+//			success = false;
+//		}
+//		model.addAttribute("updateSuccess",success);
+//		
+//		return "redirect:/product/productInfo?id="+product.getId();
+//	}
+//	
+//	
+//	@RequestMapping("/toggleProductActive")
+//	public String toggleProductActive(@RequestParam("id") String id, Model model) {
+//		String productId =id.substring(8);
+//		Product product = productService.findOne(Long.parseLong(productId));
+//		productService.toggleActive(product);
+//		model.addAttribute("product", product);
+//		return "productList";
+//	}
+//	
+//	@RequestMapping(value = "/stockUp", method = RequestMethod.GET)
+//	public String stockUp(@RequestParam("id") Long id, Model model) {
+//		Product product = productService.findOne(id);
+//		List<MemberCartItem> memberCartItemList = memberCartItemService.findAllAvailableItems(id);
+//		
+//		List<MemberCartItemWrapper> wrapperList = new ArrayList<>();
+//		for (MemberCartItem memberCartItem : memberCartItemList) {
+//			wrapperList.add(new MemberCartItemWrapper(memberCartItem.getShoppingCart().getUserRole().getUser(), memberCartItem)); 
+//		}
+//		
+//		model.addAttribute("product", product);
+//		model.addAttribute("wrapperList", wrapperList);
+//		return "stockUp";
+//	}
+//	
+//	@RequestMapping(value = "/stockUpConfirm", method = RequestMethod.GET)
+//	public String stockUpConfirm(@RequestParam("memberCartItemId") Long memberCartItemId, Model model) {
+//		
+//		MemberCartItem memberCartItem = memberCartItemService.findById(memberCartItemId);
+//		
+//		MemberCartItemWrapper memberCartItemWrapper = new MemberCartItemWrapper(memberCartItem.getShoppingCart().getUserRole().getUser(), memberCartItem);
+//		
+//		model.addAttribute("memberCartItemWrapper", memberCartItemWrapper);
+//		return "stockUpConfirm";
+//	}
 
 
 }
